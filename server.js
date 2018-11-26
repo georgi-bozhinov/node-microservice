b9cef96644dce02b90cce021f5fb53b7d8cdd8bb
georgi-bozhinov <georgi.bozhinov@sap.com>
2018-11-26 13:41:48 +0200

    Remove book endpoints, add user handling

--- server.js
+++ server.js
@@ -3,51 +3,19 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+app.get('/userinfo', (_req, res) => {
+  db.raw('SELECT current_user, session_user').then(user => {
+    let users = {};
+    users.application_user = user.rows[0].current_user;
+    users.current_user = user.rows[0].current_user;
+    users.session_user = user.rows[0].current_user;
 
-app.get('/api/addressbook/books/select', (req, res) => {
-  db.select()
-    .table('books')
-    .then(books => {
-      res.send(books);
-    })
-    .catch(err => {
-      res.status(500).send('Error while getting books!');
-    });
-});
-
-app.get('/api/addressbook/books/insert', (req, res) => {
-  const id = getRandomInt(1, 100000);
-  const name = `My Book #${id}`;
-  const book = {id, name};
-  db.insert(book)
-    .into('books')
-    .returning('id')
-    .then(id => {
-      res.json({book_id: id});
-    })
-    .catch(err => {
-      console.log(err.stack);
-      res.status(500).send('Error while inserting!');
-    });
-});
-
-app.get('/api/addressbook/books/delete', (req, res) => {
-  db('books')
-    .del()
-    .then(() => {
-      res.json({status: 'success'});
-    })
-    .catch(err => {
-      console.log(err.stack);
-      res.status(500).send('Error while deleting!');
-    });
+    res.send({ dbInfo: [users] });
+  }).catch(err => {
+    return res.status(500).send(`Error while getting user: ${err}!`);
+  });
 });
 
 app.listen(process.env.PORT || 3000);
 
-module.exports = {app};
+module.exports = { app };
